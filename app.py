41efcf099ef6267b8a4d637734f56bbd050d0500
Matt Brockman <mattbrockman@gmail.com>
2023-03-25 13:43:51 -0700

    add generateNextMove

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/generateNextMove")
+def generateNextMove():
+
+    requestJSON = request.get_json()
+
+    print('requestJSON: ', requestJSON)
+
+    return {'status': 'success', 'results': {'You kill the bear!'}}
+
+if __name__ == "__main__":
+    app.run()
